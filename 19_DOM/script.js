249f7c768e7b39652b88fd1c532b5dac56fa6617
stefanstanimirovic <stanimirovic.stefan@gmail.com>
2023-12-28 19:15:57 +0100

    DOM

--- 19_DOM/script.js
+++ 19_DOM/script.js
@@ -69,3 +69,56 @@ linkovi.forEach(link => {
 });
 
 //console.log(link);
+
+let div = e1.parentNode;
+console.log(div);
+console.log(div.childNodes);
+console.log(div.childNodes[1]);
+
+div.childNodes.forEach(elem => {
+    let tip = elem.nodeName;
+    if(tip != "#text") { // if(tip == "P" || tip == "A")
+        console.log(elem);
+    }
+});
+
+// Dodati novi div tag dokumentu
+let noviDiv = document.createElement("div");
+noviDiv.innerHTML = "Div kreiran u JS-u";
+noviDiv.style.backgroundColor = "green";
+document.body.appendChild(noviDiv);
+
+let noviNaslov = document.createElement("h3");
+noviNaslov.innerHTML = "Novi naslov";
+noviDiv.appendChild(noviNaslov);
+
+// Formirati ul listu sa stavkama čiji je sadržaj proizvoljan tekst, i dodati je div elementu.
+let lista = document.createElement("ul");
+noviDiv.append(lista);
+
+let li1 = document.createElement("li");
+li1.innerHTML = "Prva stavka liste";
+let li2 = document.createElement("li");
+li2.innerHTML = "Druga stavka liste";
+let li3 = document.createElement("li");
+li3.innerHTML = "Treca stavka liste";
+
+lista.append(li1, li2, li3);
+
+// Iz ul liste izbaciti prvu stavku.
+//lista.removeChild(li1);
+//lista.removeChild(lista.firstChild);
+lista.removeChild(lista.childNodes[0]);
+
+// U ul listi zameniti drugu stavku liste.
+let li4 = document.createElement("li");
+li4.innerHTML = "Cetvrta stavka liste";
+lista.replaceChild(li4, li3);
+
+// Dodati još jednu stavku ul listi, pri čemu je sadržaj stavke slika.
+let li5 = document.createElement("li");
+lista.appendChild(li5);
+let slika = document.createElement("img");
+slika.src = "slika.jpg";
+slika.alt = "Neka slika";
+li5.appendChild(slika);
