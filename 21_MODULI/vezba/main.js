eaf0b9d10339dd446a92712f9426fc43b9cf8b4b
stefanstanimirovic <stanimirovic.stefan@gmail.com>
2024-01-11 21:13:14 +0100

    klase

--- 21_MODULI/vezba/main.js
+++ 21_MODULI/vezba/main.js
@@ -1,5 +1,6 @@
-import { generateList, generateListItem } from "./modules/list.js";
-import { generateTable, generateTableRow, generateTableItem } from "./modules/table.js";
+/*
+import { generateList, generateItem as generateListItem } from "./modules/list.js";
+import { generateTable, generateTableRow, generateItem as generateTableItem } from "./modules/table.js";
 
 let ul = generateList(document.body);
 generateListItem(ul, "images/p1.jpg");
@@ -11,3 +12,18 @@ let tr = generateTableRow(table);
 generateTableItem(tr, "images/p1.jpg");
 generateTableItem(tr, "images/p2.jpg");
 generateTableItem(tr, "images/p3.jpg");
+*/
+
+import * as List from "./modules/list.js";
+import * as Table from "./modules/table.js";
+
+let ul = List.generateList(document.body);
+List.generateItem(ul, "images/p1.jpg");
+List.generateItem(ul, "images/p2.jpg");
+List.generateItem(ul, "images/p3.jpg");
+
+let table = Table.generateTable(document.body);
+let tr = Table.generateTableRow(table);
+Table.generateItem(tr, "images/p1.jpg");
+Table.generateItem(tr, "images/p2.jpg");
+Table.generateItem(tr, "images/p3.jpg");
---
+++ 22_KLASE/main.js
@@ -0,0 +1,24 @@
+import Auto from "./moduli/auto.js";
+
+let a1 = new Auto("Audi", "bela", true); // a1._marka, a1._boja, a1._imaKrov
+console.log(a1); 
+a1.sviraj();
+
+let a2 = new Auto("Peugeot", "plava", false); // a2._marka, a2._boja, a2._imaKrov
+console.log(a2.marka);
+console.log(a2.boja);
+console.log(a2.imaKrov);
+console.log(a2.marka);
+a2.sviraj();
+
+a2.marka = "Citroen"; // seter - pristupa mu se kao polju, a zapravo se poziva metoda
+console.log(a2.marka);
+a2.imaKrov = "blabla";
+
+a1.marka = "";
+console.log(a1.marka); 
+a1.boja = "";
+console.log(a1.boja);
+
+let a3 = new Auto("", "", "");
+console.log(a3);
