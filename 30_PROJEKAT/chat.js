d7d41095b3829d1ee7fd8d5abeecb77efbefe391
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2024-02-06 20:11:42 +0100

    3 Chatroom - addChat - Dodavanje poruke u bazu

--- 30_PROJEKAT/chat.js
+++ 30_PROJEKAT/chat.js
@@ -24,7 +24,18 @@ class Chatroom{
     }
 
     // Metod za dodavanje četova
-    
+    async addChat(mess) {
+        // Kreiranje dokumenta koji želimo da upišemo u bazu
+        let docChat = {
+            message: mess,
+            username: this.username,
+            room: this.room,
+            created_at: new Date()
+        };
+        let response = await this.chats.add(docChat); // pamti dokument u db
+        return response; // Vraća promis, na koji može da se zakači .then i .catch
+    }
+
 }
 
 let chatroom1 = new Chatroom("#js", "Stefan");
@@ -34,4 +45,12 @@ console.log(chatroom1.username);
 chatroom1.room = "#general"; // Testiram seter za room
 console.log(chatroom1.room);
 console.log(chatroom1.chats);
+
+chatroom1.addChat("Zdravo svima!")
+    .then(() => {
+        console.log("Uspešno dodata poruka");
+    })
+    .catch(err => {
+        console.log("Došlo je do greške: ", err);
+    });
 
