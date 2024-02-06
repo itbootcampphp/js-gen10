35696410b7cccd8d93c2df5a6fc4eae67a19a805
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2024-02-06 19:20:33 +0100

    3. Chatroom klasa

---
+++ 30_PROJEKAT/chat.js
@@ -0,0 +1,37 @@
+class Chatroom{
+
+    // Konstruktor
+    constructor(r, u){
+        this.room = r;
+        this.username = u;
+        this.chats = db.collection('chats');
+    }
+
+    // Seteri
+    set username(u){
+        this._username = u;
+    }
+    set room(r){
+        this._room = r;
+    }
+
+    // Geteri
+    get username(){
+        return this._username;
+    }
+    get room(){
+        return this._room;
+    }
+
+    // Metod za dodavanje četova
+    
+}
+
+let chatroom1 = new Chatroom("#js", "Stefan");
+console.log(chatroom1.username, chatroom1.room); // Testiram getere
+chatroom1.username = "Milena"; // Testiram seter za username
+console.log(chatroom1.username);
+chatroom1.room = "#general"; // Testiram seter za room
+console.log(chatroom1.room);
+console.log(chatroom1.chats);
+
